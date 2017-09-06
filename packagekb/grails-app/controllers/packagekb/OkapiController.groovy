53c5687ca53f4c0d545057b03f007ac01776e476
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-06 14:50:05 +0100

    updated

---
+++ packagekb/grails-app/controllers/packagekb/OkapiController.groovy
@@ -0,0 +1,18 @@
+package packagekb
+
+import grails.rest.*
+import grails.converters.*
+
+class OkapiController {
+	static responseFormats = ['json', 'xml']
+	
+  def index() { 
+  }
+
+  // GET And DELETE verbs with header X-Okapi-Tenant indicate activation of this module for a given tenant.
+  def tenant() {
+    println("OkapiController::tenant ${params} ${request.getHeader('X-Okapi-Tenant')}");
+    def result = [:]
+    render result as JSON
+  }
+}
