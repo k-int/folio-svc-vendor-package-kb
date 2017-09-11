2a93be2d9e21f0af09ab578c34614a33fc45d4f5
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-11 14:29:38 +0100

    Finally worked out resources in functional testing

--- packagekb/src/integration-test/groovy/packagekb/PackageOnboarding1Spec.groovy
+++ packagekb/src/integration-test/groovy/packagekb/PackageOnboarding1Spec.groovy
@@ -22,16 +22,23 @@ class PackageOnboarding1Spec extends GebSpec {
     void "Test the homepage"() {
         when:"The home page is requested"
 
+            // println("hello\n\n\n");
+            // URL[] urls = ((URLClassLoader)Thread.currentThread().getContextClassLoader()).getURLs();
+            // println(urls)
+
             // File package_file =  PackageOnboarding1Spec.class.getClassLoader().getResource("/elsevier_Global_Backfile Package - Computer Science 1995 [BFJCSC95]_2017-09-11_96497 ").file
             // File package_file = new File('src/integration-test/resources/elsevier_Global_Backfile Package - Computer Science 1995 [BFJCSC95]_2017-09-11_96497');
             // File package_file = PackageOnboarding1Spec.class.getClassLoader().getResource("/elsevier_Global_Backfile Package - Computer Science 1995 [BFJCSC95]_2017-09-11_96497 ").file
             // File package_file = Thread.currentThread().getContextClassLoader().getResource("/elsevier_Global_Backfile Package - Computer Science 1995 [BFJCSC95]_2017-09-11_96497 ").file
             // File package_file = Thread.currentThread().getContextClassLoader().getResource("/file.tsv").file;
-            File package_file = Thread.currentThread().getContextClassLoader().getResource('integration-test/resources/file.tsv').file
+            java.net.URL package_file_uri = (java.net.URL) Thread.currentThread().getContextClassLoader().getResource('file.tsv')
+            println("Got package file: ${package_file_uri.class.name} ${package_file_uri}");
+            File package_file_resource = new File(package_file_uri.toURI());
+
 
             def resp = restBuilder().post("$baseUrl/package/elsevier/admin") {
               contentType "multipart/form-data"
-              package_file = new File(package_file)
+              package_file = package_file_resource
             }
 
         then:"The response is correct"
